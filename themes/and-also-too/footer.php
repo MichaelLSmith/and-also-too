995fbd1528903af4a854cd974b0c422c79f31b21
MichaelLSmith <mlsmith45@gmail.com>
2017-02-05 18:47:46 -0500

    cleaned up footer.php

--- themes/and-also-too/footer.php
+++ themes/and-also-too/footer.php
@@ -13,34 +13,32 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="footer-container std-content-area" role="contentinfo">
-		<!-- <div class="site-footer"> -->
-			<div class="footer-middle">
-				<div class="footer-logo">
-					<div class="logo-img">Logo Here</div>
-				</div>
-				<div class="footer-social-medium">
-					<i class="fa fa-twitter-square" aria-hidden="true"></i>
-					<i class="fa fa-facebook-square" aria-hidden="true"></i>
-					<i class="fa fa-instagram" aria-hidden="true"></i>
-					<i class="fa fa-linkedin-square" aria-hidden="true"></i>
-				</div>
-
+	<footer id="colophon" class="site-footer std-content-area" role="contentinfo">
+		<div class="footer-middle">
+			<div class="footer-logo">
+				<div class="logo-img">Logo Here</div>
 			</div>
-			<div class="footer-phone">
-				<div class="contact-phone">Canada (416)628-8452</div>
-				<div class="contact-phone">USA (802)730 0091</div>
-			</div>
-			<div class="footer-social">
+			<div class="footer-social-medium">
 				<i class="fa fa-twitter-square" aria-hidden="true"></i>
 				<i class="fa fa-facebook-square" aria-hidden="true"></i>
 				<i class="fa fa-instagram" aria-hidden="true"></i>
 				<i class="fa fa-linkedin-square" aria-hidden="true"></i>
 			</div>
-			<div class="footer-email">
-				<div class="email-text">collborate@andalsotoo.net</div>
+		</div>
+
+		<div class="footer-phone">
+			<div class="contact-phone">Canada (416)628-8452</div>
+			<div class="contact-phone">USA (802)730 0091</div>
+		</div>
+		<div class="footer-social">
+			<i class="fa fa-twitter-square" aria-hidden="true"></i>
+			<i class="fa fa-facebook-square" aria-hidden="true"></i>
+			<i class="fa fa-instagram" aria-hidden="true"></i>
+			<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+		</div>
+		<div class="footer-email">
+			<div class="email-text">collborate@andalsotoo.net</div>
 			</div>
-		<!-- </div> -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
